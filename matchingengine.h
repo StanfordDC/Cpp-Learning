c2ef50e5a978fce5c7b34746c50694fbd7ae14a8
StanfordDC <stanforddchandra@gmail.com>
2022-08-20 22:27:32 +0800

    Added members and singleton constructor

--- matchingengine.h
+++ matchingengine.h
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <unordered_map>
 #include "order.h"
 
 class MatchingEngine
 {
+private: 
+    MatchingEngine() = default;
+    unordered_map<Order, int> buyOrders;
+    unordered_map<Order, int> sellOrders;
+    vector<Order> orderRecords;
+
+public:
+    //Singleton constructor
+    static MatchingEngine& getInstance(){
+        static MatchingEngine instance;
+        return instance;
+    } 
+
+    unordered_map<Order, int> getBuyOrders();
+
+    unordered_map<Order, int> getSellOrders();
+
+    vector<Order> getOrderRecords();
 
 };
